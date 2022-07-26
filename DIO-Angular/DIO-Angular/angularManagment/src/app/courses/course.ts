f9619756a74028a85ebcf38527e86ff9f7ba0faa
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-07-26 19:12:01 -0300

    adicionado ate nav-bar

---
+++ DIO-Angular/DIO-Angular/angularManagment/src/app/courses/course.ts
@@ -0,0 +1,12 @@
+export class Course{
+    id!: number;
+    name!: string;
+    price!: number;
+    imageUrl!: string;
+    code!: string;
+    duration!: number;
+    rating!: number;
+    releaseDate!: string;
+    description!:string;
+    
+}
