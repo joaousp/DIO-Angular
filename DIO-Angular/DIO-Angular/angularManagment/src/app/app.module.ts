4c750c9d18599a84c3b5648b1ca8385a396b56f3
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-07-27 16:18:18 -0300

    criando modulos separados

--- DIO-Angular/DIO-Angular/angularManagment/src/app/app.module.ts
+++ DIO-Angular/DIO-Angular/angularManagment/src/app/app.module.ts
@@ -1,40 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser'; 
 import { HttpClientModule } from '@angular/common/http';
 
 
-import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
+import { AppComponent } from './app.component';  
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './error404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { Error404Component } from './error404/error-404.component'; 
+import { CourseModule } from './courses/course-module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
+    AppComponent,  
     NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
+    Error404Component 
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule, 
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
 
-      {
-        path:'courses', component: CourseListComponent
-      },
-      {
-        path:'courses/info/:id', component:CourseInfoComponent
-      },
+     
       {
         path:'',redirectTo: 'courses', pathMatch: 'full'
       },
