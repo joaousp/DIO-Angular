a2fd4eea52e1d64fb188766a1fdb6fc1632c677a
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-07-27 00:28:21 -0300

    rotas

--- DIO-Angular/DIO-Angular/angularManagment/src/app/app.module.ts
+++ DIO-Angular/DIO-Angular/angularManagment/src/app/app.module.ts
@@ -6,17 +6,39 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+
+      {
+        path:'courses', component: CourseListComponent
+      },
+      {
+        path:'courses/info/:id', component:CourseInfoComponent
+      },
+      {
+        path:'',redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path:'**',component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
