765357fa8061d5386cba9978c1e554bf7a41d32c
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-07-27 17:32:52 -0300

    finalizado

--- DIO-Angular/DIO-Angular/angularManagment/src/app/app.module.ts
+++ DIO-Angular/DIO-Angular/angularManagment/src/app/app.module.ts
@@ -3,31 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 
-import { AppComponent } from './app.component';  
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { RouterModule } from '@angular/router';
-import { Error404Component } from './error404/error-404.component'; 
+import { AppComponent } from './app.component';   
+import { RouterModule } from '@angular/router'; 
 import { CourseModule } from './courses/course-module';
+import { CoreModule } from './core/component/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,  
-    NavBarComponent,
-    Error404Component 
+    AppComponent 
   ],
   imports: [
     BrowserModule, 
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
 
      
       {
         path:'',redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path:'**',component: Error404Component
-      }
+      } 
     ])
   ],
   providers: [],
